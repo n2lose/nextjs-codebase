c969421613725d65d6d0bca7a6cff559724f5fdd
Lam Nguyen <lam.ntu@esoft.com>
2024-04-23 11:34:22 +0700

    create default theme storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,6 @@
-import {addons} from '@storybook/addons'
-import customTheme from './theme'
+import { addons } from '@storybook/manager-api';
+import defaultTheme from './defaultTheme';
 
 addons.setConfig({
-	theme: customTheme,
-})
+  theme: defaultTheme
+});
